71f950b100483d768e2ee9619a73cf4061d074bd
Reyna Javar <rjavar@catalyte.io>
2021-07-14 08:50:12 -0700

    update app.py before_first_request function among other things

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+# https://github.com/SEO-Tech-Accelerator/flask-example-3/blob/adding_audio/routes.py
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy  # pip install Flask-SQLAlchemy
-
 from forms import RegistrationForm
+from audio import printWAV
+import time, random, threading
+from turbo_flask import Turbo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '37ac8b67e9bd5056773b58f767780a9c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+interval=10
+FILE_NAME = "Doctor_Speech.wav"
+turbo = Turbo(app)
 
 
 class User(db.Model):
@@ -45,6 +52,48 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))  # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/captions")
+def captions():
+    TITLE = "Doctor Speech"
+    return render_template('captions.html', songName=TITLE, file=FILE_NAME)
+
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w")
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions, daemon=True).start()
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
 
 
 if __name__ == '__main__':
